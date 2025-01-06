d1b5d93c547953df85ba2d71a4d5f05eac5ea984
aslan.french <thedonquixotic@gmail.com>
2025-01-05 19:14:23 -0600

    completed exercise 3

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,20 +1,99 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+export defaults {
+  data() {
+          return {
+            count: 10,
+            counterTitle: 'Counter Standard',
+            incrementAmount: 8,
+            message: 'Hello it works',
+            listOfNumbers: [
+              {
+                name: 1,
+                id: '6a887cd2-f0bf-4321-b192-92016f82a883',
+                list: [1, 2, 3]
+              },
+              {
+                name: 2,
+                id: '8d14d90b-2d47-473e-8293-d5c324111d0d',
+                list: [1, 2, 3]
+              },
+              {
+                name: 3,
+                id: 'cd806d65-2309-4625-9104-dcd636cd79b5',
+                list: [1, 2, 3]
+              },
+              {
+                name: 4,
+                id: '00c939cd-fbf4-46d0-8e61-0bc2ce8a5332',
+                list: [1, 2, 3]
+              },
+              {
+                name: 5,
+                id: '295a8170-59c1-4462-9de1-3c9cd41cedab',
+                list: [1, 2, 3]
+              }
+            ]
+          }
+        },
+        computed: {
+          displayTitle() {
+            if (this.count > 20) {
+              return 'Counter Standard - Very Long'
+            } else {
+              return 'Counter Standard'
+            }
+          },
+          optimizedIncrementAmount() {
+            return this.displayTitle.length * this.incrementAmount
+          }
+        },
+        methods: {
+          incrementCount(newAmount, event) {
+            console.log(newAmount)
+            console.log(event)
+            this.count += this.optimizedIncrementAmount
+          }
+        },
+        watch: {
+          count(newValue) {
+            if (newValue > 20) {
+              this.counterTitle += ' Very Long'
+            }
+          }
+        }
+      }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <p v-if="characterList.length === 0">There are no characters</p>
+      <ul v-else-if="characterList.length % 2 === 0">
+        <li v-for="character in characterList">
+          <p>{{ character.name }}</p>
+          <button @click="favoriteCharacter(character)">⭐ Favorite</button>
+        </li>
+      </ul>
+      <h2>Favorite Characters</h2>
+      <ul v-if="favoriteList.length > 0">
+        <li v-for="character in favoriteList">{{ character }}</li>
+        <p v-else>No favorite characters yet!</p>
+      </ul>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+      <h2>New Character</h2>
+      <pre>{{ newCharacter }}</pre>
+      <label for="character-name">Name</label>
+      <input
+        type="text"
+        v-model="newCharacter.name"
+        @keyup.enter="addNewCharacter"
+      />
+      <p>
+        <span v-for="(character, index) in characterList"
+          >{{ character.name }}{{ index === characterList.length - 1 ? '' : ', '
+          }}
+        </span>
+      </p>
+      <!-- <p v-else>There are odd characters!</p> -->
 </template>
 
 <style scoped>
